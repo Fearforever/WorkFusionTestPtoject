48e352d968a769a96075ea438f866bd50a06050b
Fearforever <Fearforever@tut.by>
2016-02-11 13:13:13 +0300

    #4

---
+++ DB_script/WFTDB.sql
@@ -0,0 +1,55 @@
+/*
+SQLyog Trial v12.18 (64 bit)
+MySQL - 5.7.11-log : Database - wftest
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`wftest` /*!40100 DEFAULT CHARACTER SET utf8 */;
+
+USE `wftest`;
+
+/*Table structure for table `email` */
+
+DROP TABLE IF EXISTS `email`;
+
+CREATE TABLE `email` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `email` varchar(50) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+
+/*Data for the table `email` */
+
+insert  into `email`(`id`,`email`) values 
+(1,'Fearforever@mail.ru'),
+(2,'Fearforever@tut.by');
+
+/*Table structure for table `rate` */
+
+DROP TABLE IF EXISTS `rate`;
+
+CREATE TABLE `rate` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `rate` float NOT NULL,
+  `date` date NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=26 DEFAULT CHARSET=utf8;
+
+/*Data for the table `rate` */
+
+insert  into `rate`(`id`,`rate`,`date`) values 
+(15,40.1342,'2016-02-09'),
+(25,41.2236,'2016-02-11');
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
